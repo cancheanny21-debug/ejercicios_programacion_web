efa7325a17cfc8856bfd651e652c510424b8887c
Anny Canche <cancheanny21@gmail.com>
2026-07-13 14:30:02 -0500

    Marices

--- Arrays y Matriz/script.js
+++ Arrays y Matriz/script.js
@@ -1,43 +1,44 @@
+MatrizEscalera();
+
 function MatrizEscalera() {
-
     let table_id = document.getElementById("matriz_id");
+
+
+    let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let colores = ["Rojo", "Funcsia", "Turquesa", "Amarillo", "Negro", "Blanco", "Cian", "Naranja", "Mangenta", "Gris"];
+    let nombres = ["David", "Ana", "Luis", "Maria", "Carlos", "Sofia", "Pedro", "Lucia", "Diego", "Elena"];
+
     let matriz = [];
-    let tabla_matriz = "";
 
-    for (let i = 0; i < 5; i++) {
 
+    let tabla_matriz = `
+        <tr> <th>ID / Número</th><th>Color Asignado</th><th>Nombre Persona</th><th>Registro de la Matriz </th></tr>
+    `
+
+    for (let i = 0; i < numeros.length; i++) {
         matriz[i] = [];
-
-        for (let j = 0; j <= 5; j++) {
-
+        for (let j = 0; j < colores.length; j++) {
             matriz[i][j] = [];
 
-            for (let k = 0; k <= 5; k++) {
+            for (let k = 0; k < nombres.length; k++) {
 
-                matriz[i][j][k] = "Jose en el campo: " + i +
-                    " registro " + j +
-                    " su valor es: " + k;
+                matriz[i][j][k] = `${nombres[k]} en el campo ${numeros[i]} registró el color ${colores[j]}`;
+
 
                 tabla_matriz += `
-                <tr>
-                    <td>${i}</td>
-                    <td>${j}</td>
-                    <td>${k}</td>
-                    <td>${matriz[i][j][k]}</td>
-                </tr>`;
+                    <tr>
+                        <td>${numeros[i]}</td>
+                        <td>${colores[j]}</td>
+                        <td>${nombres[k]}</td>
+                        <td>${matriz[i][j][k]}</td>
+                    </tr>
+                `;
             }
         }
     }
 
-    table_id.innerHTML = `
-        <tr>
-            <th>i</th>
-            <th>j</th>
-            <th>k</th>
-            <th>Dato</th>
-        </tr>
-        ${tabla_matriz}
-    `;
+    table_id.innerHTML = tabla_matriz;
 }
+
 
 MatrizEscalera();
---
+++ Calculadora/main.js
@@ -0,0 +1,20 @@
+const pantalla = document.getElementById('pantalla');
+function insertar(valor) {
+    pantalla.value += valor;
+}
+
+function limpiar() {
+    pantalla.value = "";
+}
+
+function borrar() {
+    pantalla.value = pantalla.value.slice(0, -1);
+}
+
+function calcular() {
+    try {
+        pantalla.value = eval(pantalla.value);
+    } catch (error) {
+        pantalla.value = "Error";
+    }
+}
